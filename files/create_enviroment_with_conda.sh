8f01e924bc2eb250431afe78207580b1414e3f99
Didier Barradas Bautista <didier.barradas@gmail.com>
2021-08-26 17:30:45 +0300

    code and files added

---
+++ files/create_enviroment_with_conda.sh
@@ -0,0 +1,5 @@
+conda install -c conda-forge deepchem
+conda install -y -c conda-forge rdkit
+conda install -c conda-forge jupyterlab
+conda install -c conda-forge -c ccsb-scripps vina
+conda install -c conda-forge chembl_webresource_client

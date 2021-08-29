d3adeca3feec93e0849b6ee7843c085cb3ff3f9c
barradd <didier.barradasbautista@kaust.edu.sa>
2021-08-29 16:11:51 +0300

    files edited

--- files/create_enviroment_with_conda.sh
+++ files/create_enviroment_with_conda.sh
@@ -1,5 +1,10 @@
-conda install -c conda-forge deepchem
-conda install -y -c conda-forge rdkit
+#conda install -c conda-forge deepchem
+#conda install -y -c conda-forge rdkit
+conda install -c conda-forge rdkit deepchem==2.5.0
+conda install -c conda-forge openbabel 
+pip install meeko
+pip install pdb-tools 
+#conda install -c ccsb-scripps meeko
 conda install -c conda-forge jupyterlab
 conda install -c conda-forge -c ccsb-scripps vina
 conda install -c conda-forge chembl_webresource_client
